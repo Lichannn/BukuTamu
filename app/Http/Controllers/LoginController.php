17257651f697c65736add4caa0a07adc2f87e67e
Lichannn <christmario20@gmail.com>
2024-09-02 09:26:36 +0700

    minify

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\MessageBag;
 
 use Illuminate\Http\Request;
 class LoginController extends Controller
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->web(append: [
+            \Fahlisaputra\Minify\Middleware\MinifyHtml::class,
+            \Fahlisaputra\Minify\Middleware\MinifyCss::class,
+            \Fahlisaputra\Minify\Middleware\MinifyJavascript::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
